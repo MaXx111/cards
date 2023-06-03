a040fdea93266205595d5cf68c431976a1f535f0
Sidorov Maksim <maxx11167@gmail.com>
2023-06-03 18:30:04 +0300

    feat: add css

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ function App() {
 
   return (
     <>
+      <Card src={"/src/assets/Jinx_Arcane_6_Render.webp"}>
+        <h3 className="card-title">This is JINX!</h3>
+        <p className="card-text">Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx Jinx</p>
+        <a href="https://arcane.fandom.com/wiki/Jinx" className="button">Jinx!</a>
+      </Card>
+
       <Card src={false}>
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
-        </Card>
+        <h5 className="card-title">This is not Jinx</h5>
+        <p className="card-text">Boring text</p>
+        <a href="https://www.brade.zone/2008/09/13/boring/" className="button">not jinx</a>
+      </Card>
     </>
   )
 }
